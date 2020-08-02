c49724eb4465d98046fc600977835eb5c37c508e
Butty256 <butty.main@gmail.com>
2020-08-02 12:50:52 +0900

    add comment

--- CamView.py
+++ CamView.py
@@ -11,13 +11,16 @@ if __name__ == '__main__':
     capture = cv2.VideoCapture(num, cv2.CAP_DSHOW)
 
     while(True):
+        # 画像の読み込み，失敗したらブランク画像
         ret, frame = capture.read()
         if frame is None:
             frame = numpy.zeros((9, 16, 3))
         # end
 
-        cv2.namedWindow(windowname, cv2.WINDOW_GUI_EXPANDED | cv2.WINDOW_NORMAL)
+        # Windowの生成
+        cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
 
+        # Windowと画像のresize，コメントアウト可能
         x, y, w, h = cv2.getWindowImageRect(windowname)
         gcd = math.gcd(frame.shape[1], frame.shape[0])
         wr = int(frame.shape[1] / gcd)
@@ -31,8 +34,10 @@ if __name__ == '__main__':
         cv2.resizeWindow(windowname, windowsize)
         frame = cv2.resize(frame, windowsize, cv2.INTER_AREA)
 
+        # 画像の表示
         cv2.imshow(windowname, frame)
 
+        # Escで終了，Spaceでカメラの選択
         key = cv2.waitKey(1)
         if key & 0xFF == 27:
             break
@@ -44,6 +49,7 @@ if __name__ == '__main__':
         # end
     # end
 
+    # caputureのreleaseとWindowの終了
     capture.release()
     cv2.destroyAllWindows()
 # end
